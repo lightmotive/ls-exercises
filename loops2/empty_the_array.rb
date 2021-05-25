402419ea39bb7a8f93d9f107aa8a991e39739ea1
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-25 19:29:52 +0000

    Complete Loops 2/Empty the Array exercise

--- loops2/empty_the_array.rb
+++ loops2/empty_the_array.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
-# Given the array below, use loop to remove and print each name. Stop the loop once names doesn't contain any more elements.
+# Given the array below, use loop to remove and print each name.
+# Stop the loop once names doesn't contain any more elements.
 
-names = ['Sally', 'Joe', 'Lisa', 'Henry']
+names = %w[Sally Joe Lisa Henry]
+
+loop do
+  puts names.shift
+  break if names.empty?
+end
+
+# To print in reverse, one could use names.pop.
