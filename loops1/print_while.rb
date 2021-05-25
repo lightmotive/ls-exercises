9d2fff971ea98d2ef7737ff5449444c3a7735eeb
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-25 16:28:16 +0000

    Complete Loops 1/Print While exercise

--- loops1/print_while.rb
+++ loops1/print_while.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # Using a while loop, print 5 random numbers between 0 and 99.
-# The code below shows an example of how to begin solving this exercise.
 
-numbers = []
+numbers = [27, 92, 16, 35, 10]
 
-while <condition>
-  # ...
+while numbers.length > 0
+  puts numbers.pop
 end
+# There are, of course, better ways to write that.
