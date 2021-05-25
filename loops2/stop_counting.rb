5bfa649a1b9a09712bc6e16854d9e92b9308cbda
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-25 19:33:22 +0000

    Complete Loops 2/Stop Counting exercise

--- loops2/stop_counting.rb
+++ loops2/stop_counting.rb
@@ -4,5 +4,6 @@
 # Modify the block so that it prints the current number and stops iterating when the current number equals 2.
 
 5.times do |index|
-  # ...
+  puts index
+  break if index == 2
 end
