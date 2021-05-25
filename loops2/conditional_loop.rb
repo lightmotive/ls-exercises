1dada03ee071fee2bd69f760dbf9a520cb5cd7a3
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-25 19:15:10 +0000

    Complete Loops 2/Conditional Loop exercise

--- loops2/conditional_loop.rb
+++ loops2/conditional_loop.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
-# Using an if/else statement, run a loop that prints "The loop was processed!" one time if process_the_loop equals true.
-# Print "The loop wasn't processed!" if process_the_loop equals false.
+# Using an if/else statement, run a loop that prints "The loop was processed!" one time if process_it equals true.
+# Print "The loop wasn't processed!" if process_it equals false.
 
-process_the_loop = [true, false].sample
+boolean_arr = [true, false]
+loop do
+  process_it = boolean_arr.sample
+  if process_it
+    puts 'It was processed!'
+    break
+  else
+    puts "It wasn't processed"
+  end
+end
+
+# That doesn't match Launch School's solution, but it does follow the instructions.
+# It's also closer to real-world scenario, e.g., retry until successful.
+#   A programmer would also likely include a retry_max counter and break when met.
