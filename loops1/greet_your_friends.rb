12ffb027efde70dfb349d6166fbf700dab8b0ccb
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-25 18:18:14 +0000

    Complete Loops 1/Greet Your Friends exercise

--- loops1/greet_your_friends.rb
+++ loops1/greet_your_friends.rb
@@ -4,3 +4,7 @@
 # Given the following array of names, use a for loop to greet each friend individually.
 
 friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
