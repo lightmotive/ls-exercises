7f0a156b7450f1d2999c248a308692dd8cdff17b
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-26 20:40:24 +0000

    Complete Return exercise 1

---
+++ return/01_breakfast_lunch_dinner.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  'Breakfast'
+end
+
+puts meal
+# => "Breakfast"
+# meal explicitly returns the string "Breakfast", so that's what puts puts.
