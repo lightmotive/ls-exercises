24ec16e7616c69d5366fd9b206e9f26b608b9eb7
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-26 21:34:14 +0000

    Complete Return exercise 9
    - Counting Sheep, part 3

--- return/07_counting_sheep.rb
+++ return/07_counting_sheep.rb
@@ -22,6 +22,7 @@ puts count_sheep
 #   Finally, the times method returns self, which is 5 in this case. count_sheep, therefore, returns 5.
 #   Main puts that value.
 
+puts 'Part 2:'
 # What will the following code print? Why? Don't run it until you've attempted to answer.
 
 def count_sheep
@@ -42,3 +43,25 @@ puts count_sheep
 
 # Why? The first 5 lines are the same as the previous exercise.
 # 10 is output because count_sheep implicitly returns the value of the last expression in the method: 10.
+
+puts 'Part 3:'
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+    return if sheep >= 2
+  end
+end
+
+p count_sheep
+# Output:
+# 0
+# 1
+# 2
+# nil
+
+# Why?: As usual, the times method puts 0-based indexes.
+# This time, though, it explicitly returns with a condition that stops after puts 2.
+# The return statement itself doesn't have an argument, so it returns nil to times,
+# which count_sheep returns for main to p.
