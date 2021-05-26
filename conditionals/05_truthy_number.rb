91d1d28d0f5ac383c448e68c39f4d67105fc735f
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-26 22:14:40 +0000

    Complete Conditionals exercise 5

---
+++ conditionals/05_truthy_number.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# Output:
+# My favorite number is 7.
+
+# Why?: All values in Ruby are "truthy" except nil and false.
