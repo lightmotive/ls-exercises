b6972d0be6466d51dfac2f3259ec097cf067ab72
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-26 22:33:12 +0000

    Complete Conditionals exercise 10
    - Stoplight, part 3

--- conditionals/06_stoplight.rb
+++ conditionals/06_stoplight.rb
@@ -4,19 +4,15 @@ stoplight = %w[green yellow red].sample
 
 # Write a case statement that prints "Go!" if stoplight equals 'green', "Slow down!" if stoplight equals 'yellow', and "Stop!" if stoplight equals 'red'.
 
-action = case stoplight
-         when 'green' then 'Go!'
-         when 'yellow' then 'Slow down!'
-         when 'red' then 'Stop!'
-         end
-
-puts action
+puts case stoplight
+     when 'green' then 'Go!'
+     when 'yellow' then 'Slow down!'
+     when 'red' then 'Stop!'
+     end
 
 # Convert case to if (for demonstration purposes)
 
-action = if stoplight == 'green' then 'Go!'
-         elsif stoplight == 'yellow' then 'Slow down!'
-         elsif stoplight == 'red' then 'Stop!'
-         end
-
-puts action
+puts(if stoplight == 'green' then 'Go!'
+     elsif stoplight == 'yellow' then 'Slow down!'
+     elsif stoplight == 'red' then 'Stop!'
+     end)
