950014d8ff317bb232b4cf28b6e4e6e71fd397c1
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-26 21:27:03 +0000

    Complete Return exercise 8
    - Counting Sheep, part 2

--- return/07_counting_sheep.rb
+++ return/07_counting_sheep.rb
@@ -21,3 +21,24 @@ puts count_sheep
 #   The sheep variable in the passed block would be the value of each number, which the block puts.
 #   Finally, the times method returns self, which is 5 in this case. count_sheep, therefore, returns 5.
 #   Main puts that value.
+
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+  end
+  10
+end
+
+puts count_sheep
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
+# 10
+
+# Why? The first 5 lines are the same as the previous exercise.
+# 10 is output because count_sheep returns the value of the last expression in the method: 10.
