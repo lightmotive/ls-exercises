6ceef4ebc6eac86e88ab1a2b48de0ce1e62fa667
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-27 15:26:12 +0000

    Update Strings exercise 1
    - Demonstrate string mutation w/ frozen_string_literal: true

--- strings/01_create_a_string.rb
+++ strings/01_create_a_string.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
 str = String.new
+p str
+
+# That initialization method would allow string mutation when frozen_string_literal: true,
+# which would not be possible if initialized with a literal, e.g., str = ''
+str += 'Hello world!'
+
+puts str
