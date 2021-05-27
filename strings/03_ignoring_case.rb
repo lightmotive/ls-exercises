f79cedefa5dcd3cf0df04ab6b070872e636fde3a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-27 15:48:52 +0000

    Update Strings exercise 3
    - Note about casecmp method (like combined comparison operator)

--- strings/03_ignoring_case.rb
+++ strings/03_ignoring_case.rb
@@ -4,3 +4,5 @@ name = 'Roger'
 
 p name.casecmp?('RoGeR')
 p name.casecmp?('DAVE')
+
+# There's also casecmp, which returns a value like a case-insensitive combined comparison operator (<=>), e.g., (name.downcase <=> 'RoGeR'.downcase) == 0
