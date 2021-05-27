5b490f79b3638d1fbea82f04e25d949d186f2e43
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-27 18:25:13 +0000

    Update Strings exercise 8
    - Add Launch School solution as third alternative

--- strings/08_print_the_alphabet.rb
+++ strings/08_print_the_alphabet.rb
@@ -3,10 +3,11 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 alphabet.each_char { |c| puts c }
 
-# Alt:
 puts "\nAlt 1:"
 puts alphabet.each_char.to_a
 
-# Alt:
 puts "\nAlt 2:"
 'a'.upto('z') { |c| puts c }
+
+puts "\nAlt 3 (perhaps less intuitive):"
+puts alphabet.split('')
