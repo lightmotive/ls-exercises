2a07ac4a2d2b20342efad5c778e4664c5818d48d
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-27 16:02:06 +0000

    Update Strings exercise 5
    - Demo other string concatenation methods

--- strings/05_combining_names.rb
+++ strings/05_combining_names.rb
@@ -6,3 +6,7 @@ last_name = 'Doe'
 full_name = "#{first_name} #{last_name}"
 
 puts full_name
+
+# Additional concatenation options include the String#+ and String#concat
+puts first_name + ' ' + last_name
+puts String.new.concat(first_name, 32, last_name)
