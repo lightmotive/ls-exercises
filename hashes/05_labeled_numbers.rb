2b3715c89d14b7e8cff280b827a48f25e199304a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 15:44:10 +0000

    Complete Hashes exercise 7: Low, Medium, or High?

--- hashes/05_labeled_numbers.rb
+++ hashes/05_labeled_numbers.rb
@@ -12,3 +12,7 @@ numbers.each { |k, v| puts "A #{k} number is #{v}." }
 puts "\nExercise 6:"
 half_numbers = numbers.map { |_k, v| v / 2 }
 p half_numbers
+
+puts "\nExercise 7:"
+low_numbers = numbers.select { |_k, v| v < 25 }
+p low_numbers
