15ea43d9a172f2d71492c7261ae4f1d654bef30f
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 19:22:50 +0000

    Create and explain Debugging exercise 3

---
+++ debugging/03_multiply_by_five.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# When the user inputs 10, we expect the program to print The result is 50!, but that's not the output we see. Why not?
+
+def multiply_by_five(n)
+  n * 5
+end
+
+puts 'Hello! Which number would you like to multiply by 5?'
+number = gets.chomp
+
+puts "The result is #{multiply_by_five(number)}!"
+# The problem is that number is a string; multiply_by_five requires a number.
+# Otherwise, Ruby duplicates the string 5 times.
+# Fix: convert input to a number using to_i.
