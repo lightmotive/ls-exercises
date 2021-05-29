8340280ed3fec26b77b9d7e45e940ad736ca4299
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 22:28:16 +0000

    Create and explain Debugging exercise 10

---
+++ debugging/10_warriors_and_wizards.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# We started writing an RPG game, but we already run into an error message. Find the problem and fix it.
+
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then the player picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength: 20 },
+  thief: { dexterity: 20 },
+  scout: { stamina:   20 },
+  mage: { charisma: 20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase
+
+player.merge(character_classes[input])
+
+puts 'Your character stats:'
+puts player
+
+# That program results in this error:
+# 10_warriors_and_wizards.rb:21:in `merge': no implicit conversion of nil into Hash (TypeError)
+#     from 10_warriors_and_wizards.rb:21:in `<main>'
+
+# That makes it clear that the error starts on line 21, which does the following:
+#   Take the user's input (string) to select a character class.
+#   Merge the selected character class with the player's stats.
+
+# The obvious problem (without further debugging):
+# - character_classes[input] returns nil because the hash contains only symbol keys; input is a string.
+# Fix: convert input to a symbol.
