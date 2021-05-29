52c67a18f4546d6989876fb5b94b0ce02a379afb
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 21:43:45 +0000

    Improve Debugging exercise 6
    - Replace if expressions with hash lookup

--- debugging/06_confucius_says.rb
+++ debugging/06_confucius_says.rb
@@ -5,17 +5,22 @@
 # What is the problem and how can you fix it?
 
 def get_quote(person)
-  return 'Do. Or do not. There is no try.' if person == 'Yoda'
-
-  return 'I hear and I forget. I see and I remember. I do and I understand.' if person == 'Confucius'
-
-  if person == 'Einstein'
-    'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
-  end
+  {
+    Yoda: 'Do. Or do not. There is no try.',
+    Confucius: 'I hear and I forget. I see and I remember. I do and I understand.',
+    Einstein: 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.',
+    'Dalai Lama': 'Silence is sometimes the best answer.'
+  }[person.to_sym]
 end
 
 puts 'Confucius says:'
 puts "\"#{get_quote('Confucius')}\""
+
+puts 'Yoda says:'
+puts "\"#{get_quote('Yoda')}\""
+
+puts 'Dalai Lama says:'
+puts "\"#{get_quote('Dalai Lama')}\""
 
 # The problem: there is no return statement in the first two if statements, so evaluation continues.
 # That error will occur unless the argument passed results in the last if statement being true.
