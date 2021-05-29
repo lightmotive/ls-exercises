954d83235169e07ed85281761df0f8b195a5d859
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 20:46:40 +0000

    Create and explain Debugging exercise 4

---
+++ debugging/04_pets.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Magdalena has just adopted a new pet! She wants to add her new dog, Bowser, to the pets hash. After doing so, she
+# realizes that her dogs Sparky and Fido have been mistakenly removed. Help Magdalena fix her code so that all three
+# of her dogs' names will be associated with the key :dog in the pets hash.
+
+pets = { cat: 'fluffy', dog: %w[sparky fido], fish: 'oscar' }
+
+pets[:dog] = 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>"bowser", :fish=>"oscar"}
