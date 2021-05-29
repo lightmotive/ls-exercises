bfb80eb608c4a4c3954b640f5b9e9e67abd75528
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 20:52:17 +0000

    Create and explain Debugging exercise 5

---
+++ debugging/05_even_numbers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# We want to iterate through the numbers array and return a new array containing only the even numbers.
+# However, our code isn't producing the expected output. Why not? How can we change it to produce the expected result?
+
+numbers = [5, 2, 9, 6, 3, 1, 8]
+
+even_numbers = numbers.map do |n|
+  n if n.even?
+end
+# The problem: map applies a transformation to each element of an array.
+# Fix: use the Array#select method instead.
+
+p even_numbers # expected output: [2, 6, 8]
