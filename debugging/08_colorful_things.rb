d681b6ca52fd1c732616045309ed180bfe9ca2e2
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-29 22:13:06 +0000

    Improve Debugging exercise 8
    - Break when reaching the shortest array's length

--- debugging/08_colorful_things.rb
+++ debugging/08_colorful_things.rb
@@ -2,15 +2,16 @@
 
 # The following code throws an error. Find out what is wrong and think about how you would fix it.
 
-colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black', 'sapphire blue']
 things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook', 'keyboard']
 
 colors.shuffle!
 things.shuffle!
 
 i = 0
+shortest_length = [colors.length, things.length].min
 loop do
-  break if i == colors.length
+  break if i == shortest_length # This was a great Launch School suggestion!
 
   puts "#{i.zero? ? 'I have a ' : 'And a '}#{colors[i]} #{things[i]}."
 
@@ -23,3 +24,4 @@ end
 #   - Explicitly call to_s on each array query.
 #   - Replace string concatenation (+) with string interpolation, which implicitly calls to_s on each entry.
 #   - Ensure that each array has the same length.
+#   - Stop looping after reaching the shortest array's length. That was a great Launch School suggestion!
